b4f137eb111450e438b8f50196000b37f1075a3b
songi <10thddl04@gmail.com>
2025-03-03 04:22:42 +0900

    feat: Create Search

---
+++ src/main/java/com/bovo/Bovo/modules/search/controller/SearchController.java
@@ -0,0 +1,9 @@
+package com.bovo.Bovo.modules.search.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/search")
+@RequiredArgsConstructor
+public class SearchController {
+}

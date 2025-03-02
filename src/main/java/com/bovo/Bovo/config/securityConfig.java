c7cfd5c56c9d990aad8b6349356196524569708c
jeonggwwon <gyowon1026@gmail.com>
2025-03-02 19:48:01 +0900

    Delete CorsConfig and Update securityConfig

--- src/main/java/com/bovo/Bovo/config/securityConfig.java
+++ src/main/java/com/bovo/Bovo/config/securityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity // Spring Security의 보안 설정을 활성화하는 역할
@@ -21,16 +24,30 @@ public class securityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 기능 비활성화
-                .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 페이지 비활성화
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 기능 비활성화
+                .formLogin(form -> form.disable()) // 기본 로그인 페이지 비활성화
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
                 .authorizeHttpRequests(auth -> auth // 요청(URL)에 대한 접근 제어 설정
                         .requestMatchers("/", "/error", "/static/**").permitAll() // 개발 중 임시 인증 없이 허용
                         .requestMatchers("/login", "/register").permitAll() // 로그인과 회원가입 인증 없이 허용
                         .anyRequest().authenticated() // 위 요청 외에는 인증 필요
                 )
-//                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // 개발 중 임시, 모든 요청 인증 없이 허용
+                .cors(cors -> cors.configurationSource(corsConfig()))
                 .build();
 
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:5173"); // 프론트 주소
+        corsConfiguration.addAllowedMethod("*"); //모든 HTTP 메서드 허용
+        corsConfiguration.addAllowedHeader("*"); // 모든 헤더 허용
+        corsConfiguration.setAllowCredentials(true); // JWT 토큰을 쿠키에 넣어서 전달하므로
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return urlBasedCorsConfigurationSource;
     }
 }

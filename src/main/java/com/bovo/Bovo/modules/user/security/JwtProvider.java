782ec0d0c978f29d273973dd28b3fbbe8ed6f82d
jeonggwwon <gyowon1026@gmail.com>
2025-03-02 19:56:32 +0900

    feat: Update JwtProvider

--- src/main/java/com/bovo/Bovo/modules/user/security/JwtProvider.java
+++ src/main/java/com/bovo/Bovo/modules/user/security/JwtProvider.java
@@ -9,14 +9,14 @@ import java.util.Date;
 @Component
 public class JwtProvider {
     // 토큰 발급
-    public String createJwtToken(Long userid, String SecretKey, long expireTime) {
-        return Jwts.builder()
-                .claim("userid", userid)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expireTime))
-                .signWith(SignatureAlgorithm.HS256, SecretKey)
-                .compact();
-    }
+//    public String createJwtToken(Long userid, String SecretKey, long expireTime) {
+//        return Jwts.builder()
+//                .claim("userid", userid)
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis() + expireTime))
+//                .signWith(SignatureAlgorithm.HS256, SecretKey)
+//                .compact();
+//    }
 
     // 토큰 검증 로직
 

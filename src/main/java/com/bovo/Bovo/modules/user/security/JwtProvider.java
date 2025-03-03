5bf45faa8c238748c9bee392d57cffc249e44eba
jeonggwwon <gyowon1026@gmail.com>
2025-03-03 16:51:15 +0900

    refactor: Rename import

--- src/main/java/com/bovo/Bovo/modules/user/security/JwtProvider.java
+++ src/main/java/com/bovo/Bovo/modules/user/security/JwtProvider.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component

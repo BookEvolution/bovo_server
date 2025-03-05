b3e46fae70f150fb6e2f1e20574b2f5e9de64069
jeonggwwon <gyowon1026@gmail.com>
2025-03-05 15:40:25 +0900

    refactor: Refactor Medal, securityConfig

--- src/main/java/com/bovo/Bovo/config/securityConfig.java
+++ src/main/java/com/bovo/Bovo/config/securityConfig.java
@@ -2,9 +2,6 @@ package com.bovo.Bovo.config;
 
 import com.bovo.Bovo.modules.user.security.JwtFilter;
 import com.bovo.Bovo.modules.user.security.JwtProvider;
-import com.bovo.Bovo.modules.user.service.UserService;
-import com.bovo.Bovo.modules.user.service.UserServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,17 +18,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Component
 @Configuration
 @EnableWebSecurity // Spring Security의 보안 설정을 활성화하는 역할
 public class securityConfig {
-    private final String SecretKey;
-    private final JwtProvider jwtProvider;
+    private JwtFilter jwtFilter;
 
-    public securityConfig(@Value("${jwt.secretkey}") String secretKey,
-                          JwtProvider jwtProvider) {
-        this.SecretKey = secretKey;
-        this.jwtProvider = jwtProvider;
+    public securityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
     }
 
     @Bean
@@ -47,16 +39,13 @@ public class securityConfig {
                 .formLogin(form -> form.disable()) // 기본 로그인 페이지 비활성화
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
                 .authorizeHttpRequests(auth -> { // 요청(URL)에 대한 접근 제어 설정
-                    auth.requestMatchers("/**", "/error", "/static/**").permitAll(); // 개발 중 임시 인증 없이 허용
-//                    auth.requestMatchers("/login", "/register").permitAll(); // 로그인과 회원가입 인증 없이 허용
-//                    auth.requestMatchers(HttpMethod.POST, "/**").authenticated(); // 위 요청 외에는 인증 필요
+//                    auth.requestMatchers("/**", "/error", "/static/**").permitAll(); // 개발 중 임시 인증 없이 허용
+                    auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll(); // Preflight 요청 허용
+                    auth.requestMatchers("/","/login", "/register").permitAll(); // 로그인과 회원가입 인증 없이 허용
+                    auth.requestMatchers(HttpMethod.POST, "/**").authenticated(); // 위 요청 외에는 인증 필요
                 })
-//                .sessionManagement(
-//                        sessionManagement ->
-//                                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                )// 세션 사용 x
-//                .addFilterBefore(new JwtFilter(SecretKey, jwtProvider), UsernamePasswordAuthenticationFilter.class)
-//                // Jwt 인증 필터 추가
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                // Jwt 인증 필터 추가
                 .cors(cors -> cors.configurationSource(corsConfig()))
                 .build();
 

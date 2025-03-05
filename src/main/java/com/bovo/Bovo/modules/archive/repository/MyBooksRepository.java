f8c666c0898d6645d3fb8ff6b172b06934611d0d
seozong <ajtwls1222@gmail.com>
2025-03-05 11:29:27 +0900

    comment

--- src/main/java/com/bovo/Bovo/modules/archive/repository/MyBooksRepository.java
+++ src/main/java/com/bovo/Bovo/modules/archive/repository/MyBooksRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface MyBooksRepository extends JpaRepository<MyBooks, Long> {
-    // userId 컬럼(또는 연관된 User 엔티티의 id)로 등록된 책 목록을 조회
+    // userId 컬럼(또는 연관된 User 엔티티의 id)으로 등록된 책 목록을 조회
     List<MyBooks> findByUserId(Long userId);
 }
 

591ceee05f2f134272dbb52366c3e6ee173c1989
Park JeongChan <lovecar12345@naver.com>
2025-03-07 13:50:38 +0900

    fix: Update archive_detail
    archive_detail 메모 수정 기능, 컨트롤러 업데이트, 메모dto 수정

--- src/main/java/com/bovo/Bovo/modules/archive_detail/dto/response/MemoDTO.java
+++ src/main/java/com/bovo/Bovo/modules/archive_detail/dto/response/MemoDTO.java
@@ -1,4 +1,5 @@
 package com.bovo.Bovo.modules.archive_detail.dto.response;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter
@@ -9,6 +10,9 @@ import lombok.*;
 public class MemoDTO {
     private Integer memoId;
     private String memoDate;
+
+    @JsonProperty("memo_Q")
     private String memoQ;
+    @JsonProperty("memo_A")
     private String memoA;
 }

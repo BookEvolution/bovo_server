d00ca5fd4a1ed9764deebb699013897219425d1d
oestezong <oestezong@gmail.com>
2025-03-14 09:48:20 +0900

    fix: rewards entities

--- src/main/java/com/bovo/Bovo/common/MyMissionProgress.java
+++ src/main/java/com/bovo/Bovo/common/MyMissionProgress.java
@@ -1,10 +1,10 @@
 package com.bovo.Bovo.common;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;

4185764986d2fce415270870df460429c37047b3
oestezong <oestezong@gmail.com>
2025-03-14 17:00:23 +0900

    fix: archive controller, service
    - @AuthenticationPrincipal 방식으로 변경
    - 응답 dto의 독서상태를 정의된 문자열로 맵핑
    - 별점 2배수 전달

--- src/main/java/com/bovo/Bovo/modules/archive/service/ArchiveServiceImpl.java
+++ src/main/java/com/bovo/Bovo/modules/archive/service/ArchiveServiceImpl.java
@@ -1,11 +1,13 @@
 package com.bovo.Bovo.modules.archive.service;
 
 import com.bovo.Bovo.common.MyBooks;
+import com.bovo.Bovo.common.ReadingStatus;
 import com.bovo.Bovo.modules.archive.dto.ArchiveDto;
 import com.bovo.Bovo.modules.archive.dto.ArchiveResponseDto;
 import com.bovo.Bovo.modules.archive.repository.ArchiveRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,17 +35,30 @@ public class ArchiveServiceImpl implements ArchiveService {
         List<ArchiveDto> books = myBooksList.stream()
                 .map(book -> ArchiveDto.builder()
                         .bookId(book.getId())
-                        .status(String.valueOf(book.getReadingStatus()))
+                        .status(mapReadingStatus(book.getReadingStatus())) // enum을 별도의 문자열로 맵핑
                         .cover(book.getBookCover())
                         .title(book.getBookName())
                         .author(book.getBookAuthor())
                         .startDate(book.getReadingStartDate().format(formatter))
-                        .star(book.getBookScore() != null ? book.getBookScore().intValue() : 0) // null 대비
+                        .star(book.getBookScore() != null ? book.getBookScore().multiply(BigDecimal.TWO).intValue() : 0)
                         .build())
                 .collect(Collectors.toList());
 
         return ArchiveResponseDto.builder()
                 .books(books)
                 .build();
+    }
+
+    private String mapReadingStatus(ReadingStatus status) {
+        switch (status) {
+            case READING:
+                return "ing";
+            case COMPLETED:
+                return "end";
+            case WANT_TO_READ:
+                return "wish";
+            default:
+                return "";
+        }
     }
 }

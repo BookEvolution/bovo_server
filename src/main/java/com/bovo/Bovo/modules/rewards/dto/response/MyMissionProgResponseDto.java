848401de20e5de9d1384db2260cf325c5674bade
oestezong <oestezong@gmail.com>
2025-03-14 10:17:35 +0900

    feat: Add rewards dto

--- src/main/java/com/bovo/Bovo/modules/rewards/dto/response/MyMissionProgResponseDto.java
+++ src/main/java/com/bovo/Bovo/modules/rewards/dto/response/MyMissionProgResponseDto.java
@@ -3,12 +3,14 @@ package com.bovo.Bovo.modules.rewards.dto.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)

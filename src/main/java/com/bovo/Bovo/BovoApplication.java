643c7e659e54cea1a378f4319e3c72449f2ada79
jeonggwwon <gyowon1026@gmail.com>
2025-03-29 23:30:37 +0900

    refactor: db 설정

--- src/main/java/com/bovo/Bovo/BovoApplication.java
+++ src/main/java/com/bovo/Bovo/BovoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.bovo.Bovo.common")
 public class BovoApplication {
 
 	public static void main(String[] args) {
